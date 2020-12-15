f5069d27371d4349f790f5743a047c31b24475bb
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-15 16:49:22 -0300

    Creacion de componentes y servicios

---
+++ src/app/components/buscar-imagen/buscar-imagen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buscar-imagen',
+  templateUrl: './buscar-imagen.component.html',
+  styleUrls: ['./buscar-imagen.component.css']
+})
+export class BuscarImagenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/listar-imagen/listar-imagen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-imagen',
+  templateUrl: './listar-imagen.component.html',
+  styleUrls: ['./listar-imagen.component.css']
+})
+export class ListarImagenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
